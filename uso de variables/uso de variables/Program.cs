b76163c17beea59397d04b7d00610cd51e466117
Erick Moisés Arturo Sandoval Palma <esandovalp9@miumg.edu.gt>
2025-02-13 17:59:59 -0600

    Add files via upload

---
+++ uso de variables/uso de variables/Program.cs
@@ -0,0 +1,19 @@
+﻿int edad;
+double sueldo;
+string nombre;
+bool casado;
+string estado;
+Console.WriteLine("Ingrese su edad");
+edad = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese su sueldo");
+sueldo = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ingrese su nombre");
+nombre = Console.ReadLine();
+Console.WriteLine("Ingrese su estado civil (1 para casado, 2 para soltero)");
+string estadoCivil = Console.ReadLine();
+casado = estadoCivil == "1";
+
+Console.WriteLine("Su nombre es: " + nombre);
+Console.WriteLine("Su edad es: " + edad);
+Console.WriteLine("Su sueldo es: " + sueldo);
+Console.WriteLine("Su estado civil es: " + (casado ? "casado" : "soltero"));
---
+++ usuario y contraseña/usuario y contraseña/Program.cs
@@ -0,0 +1,28 @@
+﻿string usuario = "admin";
+string contraseña = "admin";
+bool correcto = false;
+while (!correcto)
+{
+    Console.WriteLine("Ingrese su usuario");
+    string usuarioIngresado = Console.ReadLine();
+    Console.WriteLine("Ingrese su contraseña");
+    string contraseñaIngresada = Console.ReadLine();
+    if (usuarioIngresado == usuario && contraseñaIngresada == contraseña)
+    {
+        Console.WriteLine("Bienvenido");
+        correcto = true;
+    }
+    if (usuarioIngresado == usuario && contraseñaIngresada != contraseña)
+    {
+        Console.WriteLine("Contraseña incorrecta");
+    }
+    if (usuarioIngresado != usuario && contraseñaIngresada == contraseña)
+    {
+        Console.WriteLine("Usuario incorrecto");
+    }
+    if (usuarioIngresado != usuario && contraseñaIngresada != contraseña)
+    {
+        Console.WriteLine("Usuario y contraseña incorrectos");
+    }
+}
+
